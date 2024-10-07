8b5da90ca57620575f392608cf80fe391dfa63d3
architect-711 <jfjjjnqhqiriykkn@gmail.com>
2024-10-07 17:50:10 +0300

    feat: reduce authentication class

--- src/main/java/edu/architect_711/words/security/authentication/ApiKeyAuthentication.java
+++ src/main/java/edu/architect_711/words/security/authentication/ApiKeyAuthentication.java
@@ -1,28 +1,18 @@
 package edu.architect_711.words.security.authentication;
 
 import java.util.Collection;
-import java.util.List;
 
-import org.springframework.security.core.Authentication;
+import org.springframework.security.authentication.AbstractAuthenticationToken;
 import org.springframework.security.core.GrantedAuthority;
 
-public class ApiKeyAuthentication implements Authentication {
-    private boolean isAuthenticated = false;
+public class ApiKeyAuthentication extends AbstractAuthenticationToken {
     private final String API_KEY;
 
-    public ApiKeyAuthentication(boolean isAuthenticated, String API_KEY) {
-        this.isAuthenticated = isAuthenticated;
+    public ApiKeyAuthentication(final String API_KEY, final Collection<? extends GrantedAuthority> authorities) {
+        super(authorities);
+        setAuthenticated(false);
+
         this.API_KEY = API_KEY;
-    }
-
-    @Override
-    public String getName() {
-        return null;
-    }
-
-    @Override
-    public Collection<? extends GrantedAuthority> getAuthorities() {
-        return List.of(() -> "read");
     }
 
     @Override
@@ -31,23 +21,8 @@ public class ApiKeyAuthentication implements Authentication {
     }
 
     @Override
-    public Object getDetails() {
-        return null;
-    }
-
-    @Override
     public Object getPrincipal() {
-        return null;
-    }
-
-    @Override
-    public boolean isAuthenticated() {
-        return isAuthenticated;
-    }
-
-    @Override
-    public void setAuthenticated(boolean isAuthenticated) throws IllegalArgumentException {
-        this.isAuthenticated = isAuthenticated;
+        return API_KEY;
     }
     
 }
