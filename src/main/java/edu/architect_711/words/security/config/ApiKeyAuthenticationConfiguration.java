a4d93c4dd1861e03ec56e5fbcd23de0d5fdac73e
architect-711 <jfjjjnqhqiriykkn@gmail.com>
2024-10-07 17:46:06 +0300

    feat: add security configuration class

--- src/main/java/edu/architect_711/words/security/config/ApiKeyAuthenticationConfiguration.java
+++ src/main/java/edu/architect_711/words/security/config/ApiKeyAuthenticationConfiguration.java
@@ -1,40 +1,36 @@
 package edu.architect_711.words.security.config;
 
 import edu.architect_711.words.security.filter.ApiKeyAuthenticationFilter;
-import edu.architect_711.words.security.manager.ApiKeyAuthenticationManager;
-import edu.architect_711.words.security.provider.ApiKeyAuthenticationProvider;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
+@EnableWebSecurity
 @Configuration
 public class ApiKeyAuthenticationConfiguration {
+    @Autowired
+    private ApplicationContext context;
+
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    public SecurityFilterChain securityFilterChainAllAuthenticated(HttpSecurity http) throws Exception {
         return http
                 .csrf(AbstractHttpConfigurer::disable)
-                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-                .authorizeHttpRequests(request -> request.anyRequest().authenticated())
-                .addFilterAt(apiKeyAuthenticationFilter(), BasicAuthenticationFilter.class)
+                .httpBasic(AbstractHttpConfigurer::disable)
+                .formLogin(AbstractHttpConfigurer::disable)
+                .sessionManagement(session -> session
+                    .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(request -> request
+                        .anyRequest().authenticated())
+                .addFilterBefore((ApiKeyAuthenticationFilter) context.getBean("apiKeyAuthenticationFilter"), UsernamePasswordAuthenticationFilter.class)
                 .build();
     }
 
-    @Bean
-    public ApiKeyAuthenticationFilter apiKeyAuthenticationFilter() {
-        return new ApiKeyAuthenticationFilter(apiKeyAuthenticationManager());
-    }
-
-    @Bean
-    public ApiKeyAuthenticationManager apiKeyAuthenticationManager() {
-        return new ApiKeyAuthenticationManager(apiKeyAuthenticationProvider());
-    }
-
-    @Bean
-    public ApiKeyAuthenticationProvider apiKeyAuthenticationProvider() {
-        return new ApiKeyAuthenticationProvider();
-    }
 }
