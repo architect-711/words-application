4d32c023d7a97271f2788f1f447bd832427c091a
architect-711 <jfjjjnqhqiriykkn@gmail.com>
2024-10-07 17:46:37 +0300

    feat: add security api key filter

--- src/main/java/edu/architect_711/words/security/filter/ApiKeyAuthenticationFilter.java
+++ src/main/java/edu/architect_711/words/security/filter/ApiKeyAuthenticationFilter.java
@@ -1,60 +1,39 @@
 package edu.architect_711.words.security.filter;
 
-import edu.architect_711.words.security.authentication.ApiKeyAuthentication;
-import edu.architect_711.words.security.manager.ApiKeyAuthenticationManager;
-import jakarta.servlet.FilterChain;
-import jakarta.servlet.ServletException;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
-import lombok.NonNull;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Value;
+import java.io.IOException;
+
+import org.springframework.lang.NonNull;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
-import java.io.IOException;
+import edu.architect_711.words.security.manager.ApiKeyAuthenticationManager;
+import edu.architect_711.words.security.service.ApiKeyAuthenticationService;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
-@Component
+@Component @RequiredArgsConstructor
 public class ApiKeyAuthenticationFilter extends OncePerRequestFilter {
-    @Value("${test.security.key.title}") @NonNull
-    private String testSecurityKeyTitle;
     private final ApiKeyAuthenticationManager manager;
-
-    public ApiKeyAuthenticationFilter(ApiKeyAuthenticationManager manager) {
-        this.manager = manager;
-    }
+    private final ApiKeyAuthenticationService apiKeyAuthenticationService;
 
     @Override
-    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+    protected void doFilterInternal(@NonNull HttpServletRequest request, @NonNull HttpServletResponse response, @NonNull FilterChain filterChain) throws ServletException, IOException {
         try {
-            final String REQUEST_API_KEY = request.getHeader(testSecurityKeyTitle);
+            final Authentication authentication = apiKeyAuthenticationService.getAuthentication(request);
 
-            if (REQUEST_API_KEY == null) {
-                log.error("The api key is null");
-                handleUnauthorized(response);
-
-                return;
-            }
-
-            final Authentication authentication = manager.authenticate(new ApiKeyAuthentication(false, REQUEST_API_KEY));
-
-            if (!authentication.isAuthenticated()) {
-                handleUnauthorized(response);
-
-                return;
-            }
-            SecurityContextHolder.getContext().setAuthentication(authentication);
-            filterChain.doFilter(request, response);
-        } catch (BadCredentialsException badCredentialsException) {
-            handleUnauthorized(response);
+            SecurityContextHolder.getContext().setAuthentication(manager.authenticate(authentication));
+        } catch (BadCredentialsException exception) {
+            response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
         }
+        filterChain.doFilter(request, response);
     }
 
-    private void handleUnauthorized(final HttpServletResponse response) {
-        response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
-    }
 }
